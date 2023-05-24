50895df16d694e33d85b7a38b9f3dd0e5dd4d466
unknown <mdsazzadhosentamim95@gmail.com>
2023-05-24 11:40:02 +0600

    add url

---
+++ src/Calender.svelte
@@ -0,0 +1,25 @@
+<script>
+  import {Datepicker, InlineCalendar } from "svelte-calendar";
+  const theme = {
+		calendar: {
+			width: '600px',
+			shadow: '0px 0px 5px rgba(0, 0, 0, 0.25)'
+		}
+	};
+</script>
+
+<main>
+  <h1>Calender App</h1>
+  <InlineCalendar {theme} />
+  
+</main>
+<p>
+    <Datepicker />
+</p>
+
+<style>
+    p{
+        
+        padding: 5px;
+    }
+</style>
